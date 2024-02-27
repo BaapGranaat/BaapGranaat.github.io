58968901897aeacb455c986c7c69e0da5726d5d2
BaapGranaat <161502459+BaapGranaat@users.noreply.github.com>
2024-02-27 18:34:34 +0100

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,36 @@
+function newLine()
+{
+    const lines = ["Iets waarmee je iemand kan vermoorden", "Flora", "Fauna", "Pokemon", "Stad, Dorp of Land", "Een mannennaam", "Een vrouwennaam", "Iets uit de mythologie", "Iets dat in je zak past", "Iets sportgerelateerd", "Bij de slager", "Iets brandbaars", "Ninetendo gerelateerd", "Een elektronisch object", "Bekend persoon", "Iets wat je doodt als het op je valt", "Een fictief persoon", "Bij de dierentuin", "Iets wat je bij de bouwmarkt kan kopen", "Iets waardoor je van je fiets kan vallen", "Een artiest", "Een huisdier", "Iets uit de zee", "Een liedje", "Een brawl stars knokker", "Bekende nederlander", "Films", "Snoep/Chips", "Drinken alcohol", "Drinken non-alcohol", "Analoog spel", "Digitaal spel", "Basketbalspeler", "Voetbalspeler"];
+    let pick = Math.floor(Math.random() * lines.length);
+    document.getElementById("text").innerHTML = lines[pick];
+}
+
+function alphabetLetter()
+{
+    const alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+    let pickAlphabet = Math.floor(Math.random() * alphabet.length);
+    document.getElementById("startLetter").innerHTML = "Je moet beginnen met de letter: " + alphabet[pickAlphabet];
+}
+
+function removeAlphabetLetter()
+{
+    document.getElementById("startLetter").innerHTML = " ";
+}
+
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+  
+  // Close the dropdown menu if the user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  } 
